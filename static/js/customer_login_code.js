5eac8d070fcfb02608665b906d5a737af639d6fc
Anusha Tiwari <anusha20362@iiitd.ac.in>
2022-04-22 14:19:49 +0530

    css js files sorted

--- static/js/customer_login_code.js
+++ static/js/customer_login_code.js
@@ -2,7 +2,7 @@ function formSubmitted()
 {
     var e = document.getElementById("emailinput").value;
     var p = document.getElementById("passwordinput").value;
-    if (e.toLowerCase()=="customer@coldmail.com" && p=="customer123")
+    if (e.toLowerCase()=="customer@coldmail.com" && p=="c123")
         window.location.href = "customer_home.html";
     else
         window.alert("Incorrect Email Id or Password");

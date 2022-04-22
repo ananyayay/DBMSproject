9101a316551f68cb5bf9f4f9da38e8a21b28b5c7
Anusha Tiwari <anusha20362@iiitd.ac.in>
2022-04-22 13:26:00 +0530

    login to customer home

--- customer_login_code.js
+++ customer_login_code.js
@@ -3,7 +3,7 @@ function formSubmitted()
     var e = document.getElementById("emailinput").value;
     var p = document.getElementById("passwordinput").value;
     if (e.toLowerCase()=="customer@coldmail.com" && p=="customer123")
-        window.alert("Succesfully logged in");
+        window.location.href = "customer_home.html";
     else
         window.alert("Incorrect Email Id or Password");
 }

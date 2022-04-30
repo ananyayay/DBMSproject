c8aeca148e42f8807b63ca61222b91c7781b2751
ananyayay <ananyaya2002@gmail.com>
2022-04-30 18:13:33 +0530

    grants admin

---
+++ scripts/grants/create_and_insert.sql
@@ -0,0 +1,26 @@
+create table customer_users(
+	email varchar(50),
+    password varchar(20)
+);
+
+create table driver_users(
+	email varchar(50),
+    password varchar(20)
+);
+
+create table admin_users(
+	email varchar(50),
+    password varchar(20)
+);
+
+CREATE USER 'admin1@coldmail.com'@localhost identified BY 'a111';
+CREATE USER 'admin2@coldmail.com'@localhost identified BY 'a222';
+CREATE USER 'admin3@coldmail.com'@localhost identified BY 'a333';
+
+CREATE USER 'driver1@coldmail.com'@localhost identified BY 'd111';
+CREATE USER 'driver2@coldmail.com'@localhost identified BY 'd222';
+CREATE USER 'driver3@coldmail.com'@localhost identified BY 'd333';
+
+CREATE USER 'customer1@coldmail.com'@localhost identified BY 'c111';
+CREATE USER 'customer2@coldmail.com'@localhost identified BY 'c222';
+CREATE USER 'customer3@coldmail.com'@localhost identified BY 'c333';

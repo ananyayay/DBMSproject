497dd5f81e7d494c3f8c5ee38bab9e4c36756138
ananyayay <ananyaya2002@gmail.com>
2022-04-30 20:10:07 +0530

    admin grants 1

--- scripts/grants/admin_grants.sql
+++ scripts/grants/admin_grants.sql
@@ -3,3 +3,24 @@
 -- grant all on owla.* to 'admin3@coldmail.com'@localhost;
 
 
+-- for admin 1
+GRANT ALL on owla.bankaccount to 'admin1@coldmail.com'@localhost;
+GRANT ALL on owla.booking to 'admin1@coldmail.com'@localhost;
+GRANT ALL on owla.cancelledrides to 'admin1@coldmail.com'@localhost;
+GRANT ALL on owla.customers to 'admin1@coldmail.com'@localhost;
+GRANT ALL on owla.drivers to 'admin1@coldmail.com'@localhost;
+GRANT ALL on owla.location to 'admin1@coldmail.com'@localhost;
+GRANT ALL on owla.olamoneyaccount to 'admin1@coldmail.com'@localhost;
+GRANT ALL on owla.payment to 'admin1@coldmail.com'@localhost;
+GRANT ALL on owla.rentalpackages to 'admin1@coldmail.com'@localhost;
+GRANT ALL on owla.savedplaces to 'admin1@coldmail.com'@localhost;
+GRANT ALL on owla.search to 'admin1@coldmail.com'@localhost;
+GRANT ALL on owla.shared to 'admin1@coldmail.com'@localhost;
+GRANT ALL on owla.sharedbookings to 'admin1@coldmail.com'@localhost;
+GRANT ALL on owla.transactions to 'admin1@coldmail.com'@localhost;
+GRANT ALL on owla.trip to 'admin1@coldmail.com'@localhost;
+GRANT ALL on owla.vehicles to 'admin1@coldmail.com'@localhost;
+GRANT ALL on owla.message to 'admin1@coldmail.com'@localhost;
+
+
+-- for admin 2

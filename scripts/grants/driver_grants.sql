e7bcecf9c598b4a6f25de7260c09de037f0756ee
ananyayay <ananyaya2002@gmail.com>
2022-04-30 18:45:01 +0530

    customer and driver grants

---
+++ scripts/grants/driver_grants.sql
@@ -0,0 +1,21 @@
+
+GRANT ALL on owla.booking to 'driver1@coldmail.com'@localhost;
+GRANT ALL on owla.rentalpackages to 'driver1@coldmail.com'@localhost;
+GRANT ALL on owla.sharedbookings to 'driver1@coldmail.com'@localhost;
+GRANT ALL on owla.trips to 'driver1@coldmail.com'@localhost;
+GRANT ALL on owla.vehicles to 'driver1@coldmail.com'@localhost; 
+GRANT ALL on owla.drivers to 'driver1@coldmail.com'@localhost;
+
+GRANT ALL on owla.booking to 'driver2@coldmail.com'@localhost;
+GRANT ALL on owla.rentalpackages to 'driver2@coldmail.com'@localhost;
+GRANT ALL on owla.sharedbookings to 'driver2@coldmail.com'@localhost;
+GRANT ALL on owla.trips to 'driver2@coldmail.com'@localhost;
+GRANT ALL on owla.vehicles to 'driver2@coldmail.com'@localhost; 
+GRANT ALL on owla.drivers to 'driver2@coldmail.com'@localhost;
+
+GRANT ALL on owla.booking to 'driver3@coldmail.com'@localhost;
+GRANT ALL on owla.rentalpackages to 'driver3@coldmail.com'@localhost;
+GRANT ALL on owla.sharedbookings to 'driver3@coldmail.com'@localhost;
+GRANT ALL on owla.trips to 'driver3@coldmail.com'@localhost;
+GRANT ALL on owla.vehicles to 'driver3@coldmail.com'@localhost; 
+GRANT ALL on owla.drivers to 'driver3@coldmail.com'@localhost;

97a80a4aa8cbc0b26607c08da90429c22da1828f
ananyayay <ananyaya2002@gmail.com>
2022-04-30 18:30:04 +0530

    user insert statements

--- scripts/grants/create_and_insert.sql
+++ scripts/grants/create_and_insert.sql
@@ -13,14 +13,18 @@ create table admin_users(
     password varchar(20)
 );
 
-CREATE USER 'admin1@coldmail.com'@localhost identified BY 'a111';
-CREATE USER 'admin2@coldmail.com'@localhost identified BY 'a222';
-CREATE USER 'admin3@coldmail.com'@localhost identified BY 'a333';
 
-CREATE USER 'driver1@coldmail.com'@localhost identified BY 'd111';
-CREATE USER 'driver2@coldmail.com'@localhost identified BY 'd222';
-CREATE USER 'driver3@coldmail.com'@localhost identified BY 'd333';
 
-CREATE USER 'customer1@coldmail.com'@localhost identified BY 'c111';
-CREATE USER 'customer2@coldmail.com'@localhost identified BY 'c222';
-CREATE USER 'customer3@coldmail.com'@localhost identified BY 'c333';
+
+insert into customer_users values('customer1@coldmail.com','c111');
+insert into customer_users values('customer2@coldmail.com','c222');
+insert into customer_users values('customer3@coldmail.com','c333');
+
+insert into driver_users values('driver1@coldmail.com','d111');
+insert into driver_users values('driver2@coldmail.com','d222');
+insert into driver_users values('driver3@coldmail.com','d333');
+
+insert into admin_users values('admin1@coldmail.com','a111');
+insert into admin_users values('admin2@coldmail.com','a222');
+insert into admin_users values('admin3@coldmail.com','a333');
+

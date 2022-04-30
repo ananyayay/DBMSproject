4480dce39e17ee268067f832dc96135d525ce7d6
ananyayay <ananyaya2002@gmail.com>
2022-04-30 19:57:51 +0530

    admin grant all and revoke

--- scripts/grants/customer_grants.sql
+++ scripts/grants/customer_grants.sql
@@ -1,3 +1,4 @@
+-- grant for customer1
 GRANT ALL on owla.bankaccount to 'customer1@coldmail.com'@localhost;
 GRANT ALL on owla.booking to 'customer1@coldmail.com'@localhost;
 GRANT ALL on owla.savedplaces to 'customer1@coldmail.com'@localhost;
@@ -12,7 +13,7 @@ GRANT ALL on owla.cancelledrides to 'customer1@coldmail.com'@localhost;
 GRANT ALL on owla.customers to 'customer1@coldmail.com'@localhost;
 
 
-
+-- grant for customer2
 GRANT ALL on owla.bankaccount to 'customer2@coldmail.com'@localhost;
 GRANT ALL on owla.booking to 'customer2@coldmail.com'@localhost;
 GRANT ALL on owla.savedplaces to 'customer2@coldmail.com'@localhost;
@@ -27,7 +28,7 @@ GRANT ALL on owla.cancelledrides to 'customer2@coldmail.com'@localhost;
 GRANT ALL on owla.customers to 'customer2@coldmail.com'@localhost;
 
 
-
+-- grant for customer3
 GRANT ALL on owla.bankaccount to 'customer3@coldmail.com'@localhost;
 GRANT ALL on owla.booking to 'customer3@coldmail.com'@localhost;
 GRANT ALL on owla.savedplaces to 'customer3@coldmail.com'@localhost;
